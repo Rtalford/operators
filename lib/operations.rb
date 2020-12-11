511daa153128e418eb8f8c34bbe7a8712e8ca602
Reynelle <Rtalford13@gmail.com>
2020-12-10 19:52:32 -0500

    commit tree 9db4582ace936f4318d5eed40c0adfe17d870239

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
+   if speed > 60 || speed < 40 
+   true 
+   else 
+   false 
+  end 
+end 
+ 
 
 
+
+  def not_safe?(speed)
+    speed > 60 || speed < 40 ? true : false
+  end
+
+   
